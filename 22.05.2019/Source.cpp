93bfc63bcb61b06dea4bec6388ae6039e3b7eb2a
Daria Tsivako <d.tsivako@gmail.com>
2019-05-28 20:52:11 +0300

    .

--- 22.05.2019/Source.cpp
+++ 22.05.2019/Source.cpp
@@ -4,27 +4,27 @@
 
 using namespace std;
 
-Student* readFromFile(char*);
-void writeToBinary(char* binaryFile, Student* students, int number);
+Student* Read(char*);
+void Write(char* binaryFile, Student* students, int number);
 void DisplayFile(char*);
 void SystemFun();
-void InitNameFile(char*);
 
 int main()
 {
 	char fileName[] = "C:/Users/Lenovo/Documents/vs/22.05.2019 para/text.txt";
 	char binaryFile[N] = "binary.bin";
-	Student* students = new Student[9];
-	students = readFromFile(fileName);
+	Student* student = new Student[9];
+	student = Read(fileName);
 	
-	writeToBinary(binaryFile, students, 9);
+	Write(binaryFile, student, 9);
 	DisplayFile(binaryFile);
+	cout << endl;
 
 	system("pause");
 }
 
 
-Student* readFromFile(char* fileName)
+Student* Read(char* fileName)
 {
 	ifstream streamIn;
 	streamIn.open(fileName);
@@ -37,9 +37,9 @@ Student* readFromFile(char* fileName)
 	}
 
 	Student* student = new Student[9];
-	char name[20];
-	char secondName[20];
-	char faculty[20];
+	char name[30];
+	char secondName[30];
+	char faculty[30];
 	int marks[4];
 
 	for (int i = 0; i < 9; i++)
@@ -47,10 +47,10 @@ Student* readFromFile(char* fileName)
 		streamIn >> name;
 		streamIn >> secondName;
 		streamIn >> faculty;
+		
 		for (int j = 0; j < 4; j++)
 		{
 			streamIn >> marks[j];
-
 		}
 
 		student[i].SetStudent(name, secondName, faculty, marks);
@@ -59,7 +59,7 @@ Student* readFromFile(char* fileName)
 }
 
 
-void writeToBinary(char* binaryFile, Student* student, int n)
+void Write(char* binaryFile, Student* student, int n)
 {
 	ofstream streamOut;
 	streamOut.open(binaryFile, ios::binary);
@@ -106,14 +106,5 @@ void SystemFun()
 {
 	cout << endl;
 	system("pause");
-	system("cls");
-}
-
-void InitNameFile(char* fileName)
-{
-	cout << "Enter the name of file: " << endl;
-	cin.ignore();
-	cin.getline(fileName, 256, '\n');
-	
 	system("cls");
 }
